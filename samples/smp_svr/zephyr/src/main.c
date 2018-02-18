59210e372c927e26637696f2198ff2efe8ea6897
Christopher Collins <ccollins@apache.org>
2018-02-18 14:42:53 -0800

    (Zephyr) Use updated stats api.
    The Zephyr stats API now requires `;` at the end of `STATS_SECT_END` and
    `STATS_NAME_END()`.

--- samples/smp_svr/zephyr/src/main.c
+++ samples/smp_svr/zephyr/src/main.c
@@ -34,12 +34,12 @@
 /* Define an example stats group; approximates seconds since boot. */
 STATS_SECT_START(smp_svr_stats)
     STATS_SECT_ENTRY(ticks)
-STATS_SECT_END
+STATS_SECT_END;
 
 /* Assign a name to the `ticks` stat. */
 STATS_NAME_START(smp_svr_stats)
     STATS_NAME(smp_svr_stats, ticks)
-STATS_NAME_END(smp_svr_stats)
+STATS_NAME_END(smp_svr_stats);
 
 /* Define an instance of the stats group. */
 STATS_SECT_DECL(smp_svr_stats) smp_svr_stats;
