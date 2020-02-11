1f9d4da3b8a7dd6a730924ecbcb387be6631ba00
de-nordic <56024351+de-nordic@users.noreply.github.com>
2020-02-11 18:57:20 +0000

    samples/smp_svr: Update of Zephyr sample (#62)
    The Zephyr version of mcumgr/smp_svr sample has been updated with current
    (commit d380622a) version of source code from Zephyr repository:
     zephyrpoject-rots/zephyr/tree/master/samples/subsys/mgmt/mcumgr/smp_svr
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- samples/smp_svr/zephyr/src/main.c
+++ samples/smp_svr/zephyr/src/main.c
@@ -8,18 +8,14 @@
 #include <zephyr.h>
 #include <string.h>
 #include <stdlib.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/conn.h>
-#include <bluetooth/gatt.h>
 #include <stats/stats.h>
-#include <mgmt/smp_bt.h>
 #include <mgmt/buf.h>
 
 #ifdef CONFIG_MCUMGR_CMD_FS_MGMT
 #include <device.h>
 #include <fs/fs.h>
-#include <nffs/nffs.h>
 #include "fs_mgmt/fs_mgmt.h"
+#include <fs/littlefs.h>
 #endif
 #ifdef CONFIG_MCUMGR_CMD_OS_MGMT
 #include "os_mgmt/os_mgmt.h"
@@ -31,8 +27,12 @@
 #include "stat_mgmt/stat_mgmt.h"
 #endif
 
-#define DEVICE_NAME         CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN     (sizeof(DEVICE_NAME) - 1)
+#ifdef CONFIG_MCUMGR_SMP_BT
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/gatt.h>
+#include <mgmt/smp_bt.h>
+#endif
 
 /* Define an example stats group; approximates seconds since boot. */
 STATS_SECT_START(smp_svr_stats)
@@ -48,14 +48,17 @@ STATS_NAME_END(smp_svr_stats);
 STATS_SECT_DECL(smp_svr_stats) smp_svr_stats;
 
 #ifdef CONFIG_MCUMGR_CMD_FS_MGMT
-static struct nffs_flash_desc flash_desc;
-
-static struct fs_mount_t nffs_mnt = {
-	.type = FS_NFFS,
-	.mnt_point = "/nffs",
-	.fs_data = &flash_desc,
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(cstorage);
+static struct fs_mount_t littlefs_mnt = {
+	.type = FS_LITTLEFS,
+	.fs_data = &cstorage,
+	.storage_dev = (void *)DT_FLASH_AREA_STORAGE_ID,
+	.mnt_point = "/lfs"
 };
 #endif
+
+#ifdef CONFIG_MCUMGR_SMP_BT
+static struct k_work advertise_work;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -64,18 +67,13 @@ static const struct bt_data ad[] = {
 		      0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
 };
 
-static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-};
-
-static void advertise(void)
+static void advertise(struct k_work *work)
 {
 	int rc;
 
 	bt_le_adv_stop();
 
-	rc = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
-			     sd, ARRAY_SIZE(sd));
+	rc = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (rc) {
 		printk("Advertising failed to start (rc %d)\n", rc);
 		return;
@@ -87,7 +85,7 @@ static void advertise(void)
 static void connected(struct bt_conn *conn, u8_t err)
 {
 	if (err) {
-		printk("Connection failed (err %u)\n", err);
+		printk("Connection failed (err 0x%02x)\n", err);
 	} else {
 		printk("Connected\n");
 	}
@@ -95,8 +93,8 @@ static void connected(struct bt_conn *conn, u8_t err)
 
 static void disconnected(struct bt_conn *conn, u8_t reason)
 {
-	printk("Disconnected (reason %u)\n", reason);
-	advertise();
+	printk("Disconnected (reason 0x%02x)\n", reason);
+	k_work_submit(&advertise_work);
 }
 
 static struct bt_conn_cb conn_callbacks = {
@@ -113,12 +111,12 @@ static void bt_ready(int err)
 
 	printk("Bluetooth initialized\n");
 
-	advertise();
+	k_work_submit(&advertise_work);
 }
+#endif
 
 void main(void)
 {
-	struct device *flash_dev;
 	int rc;
 
 	rc = STATS_INIT_AND_REG(smp_svr_stats, STATS_SIZE_32, "smp_svr_stats");
@@ -126,17 +124,9 @@ void main(void)
 
 	/* Register the built-in mcumgr command handlers. */
 #ifdef CONFIG_MCUMGR_CMD_FS_MGMT
-	flash_dev = device_get_binding(CONFIG_FS_NFFS_FLASH_DEV_NAME);
-	if (!flash_dev) {
-        printk("Error getting NFFS flash device binding\n");
-	} else {
-		/* set backend storage dev */
-		nffs_mnt.storage_dev = flash_dev;
-
-		rc = fs_mount(&nffs_mnt);
-		if (rc < 0) {
-			printk("Error mounting nffs [%d]\n", rc);
-		}
+	rc = fs_mount(&littlefs_mnt);
+	if (rc < 0) {
+		printk("Error mounting littlefs [%d]\n", rc);
 	}
 
 	fs_mgmt_register_group();
@@ -151,6 +141,9 @@ void main(void)
 	stat_mgmt_register_group();
 #endif
 
+#ifdef CONFIG_MCUMGR_SMP_BT
+	k_work_init(&advertise_work, advertise);
+
 	/* Enable Bluetooth. */
 	rc = bt_enable(bt_ready);
 	if (rc != 0) {
@@ -161,12 +154,13 @@ void main(void)
 
 	/* Initialize the Bluetooth mcumgr transport. */
 	smp_bt_register();
+#endif
 
 	/* The system work queue handles all incoming mcumgr requests.  Let the
 	 * main thread idle while the mcumgr server runs.
 	 */
 	while (1) {
-		k_sleep(1000);
+		k_sleep(K_MSEC(1000));
 		STATS_INC(smp_svr_stats, ticks);
 	}
 }
