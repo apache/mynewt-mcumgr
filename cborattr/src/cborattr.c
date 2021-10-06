18c628dfee727faca25aebfaf130f4e93538dd82
Even Falch-Larsen <even.falch.larsen@nomono.co>
2021-10-06 10:46:28 +0200

    cborattr: fixes missing error handling
    In the while loop in the function cbor_internal_read_object
    there is no error handling when the cbor_value_advance
    reports an error. This may end up in a never ending loop.
    
    Signed-off-by: Even Falch-Larsen <even.falch.larsen@nomono.co>

--- cborattr/src/cborattr.c
+++ cborattr/src/cborattr.c
@@ -306,7 +306,7 @@ cbor_internal_read_object(CborValue *root_value,
                 err |= CborErrorIllegalType;
             }
         }
-        cbor_value_advance(&cur_value);
+        err = cbor_value_advance(&cur_value);
     }
     if (!err) {
         /* that should be it for this container */
